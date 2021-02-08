e0b951ea0db5b861da21c85ce579abc322530c9d
halil-lnr <78182710+halil-lnr@users.noreply.github.com>
2021-02-08 17:35:43 -0500

    Update security_checks.sh

--- myScripts/security_checks.sh
+++ myScripts/security_checks.sh
@@ -1,19 +1,19 @@
 # Create brand new security_checks_log file.
 rm -rf security_checks_log.txt
-touch security_checks_log.txt
+touch target/security_checks_log.txt
 
 # Run security check
-mvn com.redhat.victims.maven:security-versions:check >> security_checks_log.txt
+mvn com.redhat.victims.maven:security-versions:check >> target/security_checks_log.txt
 
 # Check for security vulnerability
-if grep -q "is vulnerable to" security_checks_log.txt
+if grep -q "is vulnerable to" target/security_checks_log.txt
 then
   echo "Yes, security vulnerabilities found."
   exit 1
 fi
 
 # Display security_logs
-cat security_checks_log.txt
+cat target/security_checks_log.txt
 
 # Delete security check log file
-rm security_checks_log.txt
+rm target/security_checks_log.txt
