9beeccb8df387f5c9821d4be039bb4bd466f1d78
halil-lnr <78182710+halil-lnr@users.noreply.github.com>
2021-03-05 10:49:28 -0500

    Update sampleSteps.java

--- src/test/java/stepDefinitions/sampleSteps.java
+++ src/test/java/stepDefinitions/sampleSteps.java
@@ -112,11 +112,11 @@ public class sampleSteps {
 		
 		
 		        
-//        try {
-//          driver = new RemoteWebDriver( new URL("http://selenium__standalone-chrome:4444/wd/hub"), options);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+       try {
+         driver = new RemoteWebDriver( new URL("http://selenium__standalone-chrome:4444/wd/hub"), options);
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
 
 		
  		driver = new ChromeDriver(chromeOptions);
